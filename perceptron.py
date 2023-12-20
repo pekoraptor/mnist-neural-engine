a09da48279159eefe0110209470a5c8c5f093f45
fryniewi <01178583@pw.edu.pl>
2023-12-20 15:21:37 +0100

    random code

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 class Perceptron:
@@ -21,7 +22,30 @@ class Layer:
 
     def backward(self, y_gradient, learning_rate):
         # update weights and bias using SGD and return input gradient
-        pass
+        output_value = np.dot(y_gradient, self.weights.T)
+        weights_gradient = np.dot(y_gradient, self.input.T)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * y_gradient
+        return output_value
+
+    def _update_weights_with_sgd(self, y_gradient, learning_rate):
+        weights_gradient = np.dot(y_gradient, self.weights.T)
+        height, width = np.shape(weights_gradient)
+        to_be_updated = []
+        for i in range(height*width):
+            to_be_updated.append(1 if i < height*width / 2 else 0)
+        to_be_updated = np.array(random.shuffle(to_be_updated)).reshape(height, width)
+        for i in range(height):
+            for j in range(width):
+                self.weights[i][j] -= learning_rate * weights_gradient[i][j] * to_be_updated[i][j]
+
+    def _update_bias_with_sgd(self, y_gradient, learning_rate):
+        to_be_changed = []
+        for i in range(len(self.bias)):
+            to_be_changed.append(1 if i < len(self.bias) / 2 else 0)
+        random.shuffle(to_be_changed)
+        for i in range(len(self.bias)):
+            self.bias[i] -= learning_rate * y_gradient[i] * to_be_changed[i]
 
 
 def relu(x):
